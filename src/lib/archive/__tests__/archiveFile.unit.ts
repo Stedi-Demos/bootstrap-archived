cb56bb87943b6747ff0e60551d44ee6476830a2c
Ross Williams <ross@stedi.com>
2023-02-23 15:52:45 +0000

    fix: default execution bucket env var

--- src/lib/archive/__tests__/archiveFile.unit.ts
+++ src/lib/archive/__tests__/archiveFile.unit.ts
@@ -2,6 +2,7 @@ import test from "ava";
 import { archiveFile } from "../archiveFile.js";
 import { reset, set } from "mockdate";
 import { mockBucketClient } from "../../testing/testHelpers.js";
+import { requiredEnvVar } from '../../environment.js';
 
 const buckets = mockBucketClient();
 
@@ -13,7 +14,7 @@ test("archives file using date specific folder structure, and timestamp suffix",
   await archiveFile({ currentKey: "foo", body: "bar" });
 
   t.like(buckets.calls()[0].args[0].input, {
-    bucketName: "ab33fa45-c67a-4653-bf5a-0bb85a90b332-executions",
+    bucketName: requiredEnvVar("EXECUTIONS_BUCKET_NAME"),
     key: "archive/2023/05/25/foo_2023-05-25T18:09:12.451Z",
   });
 });

d2f7813c6b39e904e1abcbd6f1f1f5fdf9f9280d
Brian Quinn <briandquinn@gmail.com>
2023-02-23 14:53:58 +0000

    fix: remove unneeded logging mock

--- src/lib/testing/testHelpers.ts
+++ src/lib/testing/testHelpers.ts
@@ -5,27 +5,9 @@ import {
 } from "@stedi/sdk-client-buckets";
 import { StashClient } from "@stedi/sdk-client-stash";
 import { mockClient } from "aws-sdk-client-mock";
-import nock from "nock";
 import { requiredEnvVar } from "../environment.js";
 
 const executionsBucket = requiredEnvVar("EXECUTIONS_BUCKET_NAME") ?? "";
-
-/**
- * Mocks all calls to Axiom
- *
- * @param doNotMockLogMessages - array of message strings to EXCLUDE from mock
- */
-export const mockLogging = (doNotMockLogMessages: string[] = []) => {
-  // mock logging calls
-  nock(/https:\/\/cloud.axiom.co/)
-    .post(/\/api\/v1\/datasets\/.*/, (body) => {
-      const willMock = !doNotMockLogMessages.includes(body.message);
-      // console.log({ willMock });
-      return willMock;
-    })
-    .reply(200, { info: 0 })
-    .persist();
-};
 
 /**
  * Creates a mocked Stedi BucketsClient

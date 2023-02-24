95d8603cf9ab4569a732d073eb353d8e30536b6a
Brian Quinn <briandquinn@gmail.com>
2023-02-24 09:57:10 +0000

    feat: improve how dotenv is used to avoid deploy config affecting tests

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,8 +1,4 @@
-import dotenv from "dotenv";
-
 import { requiredEnvVar } from "./environment.js";
-
-dotenv.config({ override: true });
 
 export const DEFAULT_SDK_CLIENT_PROPS = {
   apiKey: requiredEnvVar("STEDI_API_KEY"),

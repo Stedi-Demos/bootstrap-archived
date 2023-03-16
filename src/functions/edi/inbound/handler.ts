3fe58b95f42a3f29b7b40d873591b45438f6be67
Brian Quinn <briandquinn@gmail.com>
2023-03-16 09:24:55 +0000

    reinstate execution tracking for inbound handler

--- src/functions/edi/inbound/handler.ts
+++ src/functions/edi/inbound/handler.ts
@@ -23,72 +23,73 @@ const buckets = bucketsClient();
 export const handler = async (event: any): Promise<Record<string, any>> => {
   console.log(JSON.stringify(event, null, 2));
   const executionId = generateExecutionId(event);
-
-  // parse the incoming event against the TransactionEvent schema
-  const parseResult = TransactionEventSchema.safeParse(event);
-
-  // skip processing anything that doesn't match (including non-transaction events)
-  if (!parseResult.success) {
-    console.log("invalid event", parseResult);
-    return {};
-  }
-
-  const transactionEvent = parseResult.data;
-
-  // skip EDI we're sending (this will be excluded by better EventBrdige rule pattern in future)
-  if (transactionEvent.detail.direction === "SENT") return {};
-
-  // load the translated Guide JSON from the bucket
-  const getObjectResponse = await buckets.send(
-    new GetObjectCommand({
-      bucketName: transactionEvent.detail.output.bucketName,
-      key: transactionEvent.detail.output.key,
-    })
-  );
-
-  // grab the Guide JSON that engine output for this incoming file
-  const fileContents = await consumers.text(getObjectResponse.body as Readable);
-  const guideJSON = JSON.parse(fileContents);
-
-  // get the destinations for this transaction set
-  const { destinations } = await loadDestinations({
-    partnershipId: transactionEvent.detail.partnership.partnershipId,
-    transactionSetIdentifier:
-      transactionEvent.detail.transaction.transactionSetIdentifier,
-  });
-
-  // prepare delivery payloads
-  const filenamePrefix = transactionEvent.detail.metadata.interchange
-    .controlNumber
-    ? transactionEvent.detail.metadata.interchange.controlNumber
-    : Date.now();
-
-  const destinationFilename = generateDestinationFilename(
-    filenamePrefix.toString(),
-    transactionEvent.detail.transaction.transactionSetIdentifier,
-    "json"
-  );
-
-  const processDeliveriesInput: ProcessDeliveriesInput = {
-    destinations,
-    payload: guideJSON,
-    destinationFilename,
-  };
-
-  // await destination deliveries
-  await processDeliveries(processDeliveriesInput);
-
-  // Delete the input file (could also archive elsewhere if desired)
-  // TODO: source object is needed in event
-  // await buckets.send(
-  //   new DeleteObjectCommand({
-  //     bucketName: transactionEvent.detail.input.bucketName,
-  //     key: transactionEvent.detail.input.key,
-  //   })
-  // );
-
   try {
     await recordNewExecution(executionId, event);
+
+    // parse the incoming event against the TransactionEvent schema
+    const parseResult = TransactionEventSchema.safeParse(event);
+
+    // skip processing anything that doesn't match (including non-transaction events)
+    if (!parseResult.success) {
+      console.log("invalid event", parseResult);
+      return {};
+    }
+
+    const transactionEvent = parseResult.data;
+
+    // skip EDI we're sending (this will be excluded by better EventBrdige rule pattern in future)
+    if (transactionEvent.detail.direction === "SENT") return {};
+
+    // load the translated Guide JSON from the bucket
+    const getObjectResponse = await buckets.send(
+      new GetObjectCommand({
+        bucketName: transactionEvent.detail.output.bucketName,
+        key: transactionEvent.detail.output.key,
+      })
+    );
+
+    // grab the Guide JSON that engine output for this incoming file
+    const fileContents = await consumers.text(
+      getObjectResponse.body as Readable
+    );
+    const guideJSON = JSON.parse(fileContents);
+
+    // get the destinations for this transaction set
+    const { destinations } = await loadDestinations({
+      partnershipId: transactionEvent.detail.partnership.partnershipId,
+      transactionSetIdentifier:
+        transactionEvent.detail.transaction.transactionSetIdentifier,
+    });
+
+    // prepare delivery payloads
+    const filenamePrefix = transactionEvent.detail.metadata.interchange
+      .controlNumber
+      ? transactionEvent.detail.metadata.interchange.controlNumber
+      : Date.now();
+
+    const destinationFilename = generateDestinationFilename(
+      filenamePrefix.toString(),
+      transactionEvent.detail.transaction.transactionSetIdentifier,
+      "json"
+    );
+
+    const processDeliveriesInput: ProcessDeliveriesInput = {
+      destinations,
+      payload: guideJSON,
+      destinationFilename,
+    };
+
+    // await destination deliveries
+    await processDeliveries(processDeliveriesInput);
+
+    // Delete the input file (could also archive elsewhere if desired)
+    // TODO: source object is needed in event
+    // await buckets.send(
+    //   new DeleteObjectCommand({
+    //     bucketName: transactionEvent.detail.input.bucketName,
+    //     key: transactionEvent.detail.input.key,
+    //   })
+    // );
 
     await markExecutionAsSuccessful(executionId);
 

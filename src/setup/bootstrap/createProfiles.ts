85b22544a7b0b02bbde6ace979b7f85829c632be
Rob Sioss <39536918+rsioss@users.noreply.github.com>
2023-03-22 11:29:01 -0400

    feat: remove beta partner profile support

--- src/setup/bootstrap/createProfiles.ts
+++ src/setup/bootstrap/createProfiles.ts
@@ -1,9 +1,6 @@
-import { CreateX12ProfileCommand } from "@stedi/sdk-client-partners";
-
 import { PartnerProfileSchema } from "../../lib/types/PartnerRouting.js";
 import { SetValueCommand } from "@stedi/sdk-client-stash";
 import { PARTNERS_KEYSPACE_NAME } from "../../lib/constants.js";
-import { partnersClient } from "../../lib/clients/partners.js";
 import { stashClient } from "../../lib/clients/stash.js";
 
 export const createProfiles = async () => {
@@ -26,41 +23,21 @@ export const createProfiles = async () => {
     },
   ];
 
-  if (process.env["USE_BETA"] === "true") {
-    const partners = partnersClient();
-    console.log("[BETA] Creating X12 Trading Partner Profile in Partners API");
+  console.log("Creating X12 Trading Partner Profile in Stash");
+  const stash = stashClient();
 
-    for (const profile of profiles) {
-      try {
-        await partners.send(new CreateX12ProfileCommand(profile));
-      } catch (error) {
-        if (
-          typeof error === "object" &&
-          error !== null &&
-          "name" in error &&
-          error.name === "ResourceConflictException"
-        )
-          console.log("Partner profile already exists");
-        else throw error;
-      }
-    }
-  } else {
-    console.log("Creating X12 Trading Partner Profile in Stash");
-    const stash = stashClient();
+  for (const profile of profiles) {
+    const parseResult = PartnerProfileSchema.safeParse(profile);
 
-    for (const profile of profiles) {
-      const parseResult = PartnerProfileSchema.safeParse(profile);
+    if (!parseResult.success) throw new Error(parseResult.error.message);
+    // throw new Error(`Invalid profile for ${profile.id}`);
 
-      if (!parseResult.success) throw new Error(parseResult.error.message);
-      // throw new Error(`Invalid profile for ${profile.id}`);
-
-      await stash.send(
-        new SetValueCommand({
-          keyspaceName: PARTNERS_KEYSPACE_NAME,
-          key: `profile|${profile.id}`,
-          value: parseResult.data,
-        })
-      );
-    }
+    await stash.send(
+      new SetValueCommand({
+        keyspaceName: PARTNERS_KEYSPACE_NAME,
+        key: `profile|${profile.id}`,
+        value: parseResult.data,
+      })
+    );
   }
 };
